5a4406da929b3cf730ba4f868a19f3754e1a8be7
Bulatovic Vuko <bulatovic.vuko@gmail.com>
2024-01-27 19:30:15 +0100

    hranaZaPseiMacke

---
+++ ujs.lab08.js
@@ -0,0 +1,12 @@
+function calculate(dogPackages, catPackages){
+    dogPackages *= 2.50;
+    catPackages *= 4;
+    let total = dogPackages + catPackages;
+    console.log(total + " USD.");
+}
+
+
+let psecaHrana = 13;
+let macecaHrana = 9;
+
+calculate(psecaHrana,macecaHrana);
